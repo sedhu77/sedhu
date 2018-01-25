8ccb6ae4db102c82f80478b76a95b34a03d73122
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-01-25 17:03:22 +0530

    Create sed.c

---
+++ sed.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+char x,A,a,E,e,I,i,O,o,U,u;
+printf("enter the character");
+scanf("%c",&x);
+if(x=='A'||x=='E'||x=='I'||x=='O'||x=='U'||x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+printf("It is vowel");
+else
+printf("It is consanant");
+}
