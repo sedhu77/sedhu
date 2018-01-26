2722600aac25f4f1e09b68dbe2eb5684c7cb2d48
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-01-26 09:02:06 +0530

    Update sed.c

--- sed.c
+++ sed.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 void main()
 {
-char x,A,a,E,e,I,i,O,o,U,u;
+char x,a,e,i,o,u;
 printf("enter the character");
 scanf("%c",&x);
 if(x=='A'||x=='E'||x=='I'||x=='O'||x=='U'||x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
