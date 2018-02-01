43e8ef1e5d041476b0b3d17f206712b3c5392b25
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-02-01 16:04:09 +0530

    Create c.c

---
+++ c.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char c;
+    scanf("%c",&c);
+
+    if( (c>='a' && c<='z') || (c>='A' && c<='Z'))
+        printf(" this Alphabet");
+    else
+printf("No");
+}
