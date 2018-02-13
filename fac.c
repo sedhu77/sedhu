716f317b11169c190b0ea6fe0a7dc2bbd8db6ac6
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-02-13 02:47:13 -0800

    Create fac.c

---
+++ fac.c
@@ -0,0 +1,14 @@
+
+#include<stdio.h>
+void main()
+{
+    int a,i,n;
+    printf("enter the values:");
+    scanf("%d",&n);
+    a=n;
+    for(i=1;i<n;i++)
+    {
+        a=a*i;
+        printf("%d",a);
+    }
+}
