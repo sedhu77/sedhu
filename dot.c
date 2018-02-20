91dd7a2154b8b5c1d82e129ca43835e2122e50e5
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-02-20 16:38:59 +0530

    Create dot.c

---
+++ dot.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    printf("enter any letter:");
+	char a[100];int count=0;
+	gets(a);
+	while(a[count]!='\0')
+	{
+		count++;
+	}
+	a[count]='.';
+	printf("%sf",a);
+	
+}
