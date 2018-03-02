9fc972eaa7fe47193793fe1755c2f245071a7b78
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-03-02 14:30:55 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  int a;
+  printf("enter the string:");
+  scanf("%d",&n);
+  if(a=0&&a<1000)
+      printf("it is  not numeric");
+      else
+      {
+      printf("it is  numeric");
+  }
+}
