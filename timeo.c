51f1f1143110a3d185de6552b1a2d03706416e5b
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-03-15 03:18:15 -0700

    Create timeo.c

---
+++ timeo.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,e;
+    printf("enter the time:");
+    scanf("%d:%d",&a);
+    printf("enter the time:");
+    scanf("%d:%d",&b);
+   e=a-b;
+   printf("%d",e);
+   
+}
