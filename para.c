d49847a9128585b1a199f705ae31891b09fe8a08
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-03-16 06:47:16 -0400

    Rename dot.c to para.c

---
+++ para.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    char s[50],i,b=0;
+    printf("enter the word:");
+    gets(s);
+    for(i=0;s[i]!='\0';i++)
+    {
+    if(s[i]=='.')
+    {
+        b++;
+    }
+    }
+    printf("%d",b);
+    
+}
+
