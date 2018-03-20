5fb2ffd31a611d8b56e249a28eed9871f5151b93
sedhu77 <35728097+sedhu77@users.noreply.github.com>
2018-03-20 04:09:00 -0700

    Create g.c

---
+++ g.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int a[10],i,n,max=a[0];
+    printf("enter the value n:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+        max=a[i];
+        }
+    }
+    printf("%d",max);
+}
